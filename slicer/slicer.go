645040e690d2d6ffb073831625c09151255579fc
MatanSilver <matansilver@gmail.com>
2017-01-11 23:15:22 -0500

    made slicer form lists of lines for each layer

--- slicer/slicer.go
+++ slicer/slicer.go
@@ -2,18 +2,26 @@ package slicer
 import (
   "../geometry"
   "../utils"
-  "fmt"
+  //"fmt"
+  "strconv"
 )
+
 func Slice(filename string, model *geometry.Model, conf *utils.Config) {
   heighestz := model.HeighestZ()
+  var linelist []geometry.LineSegment
+  iterator := 0
   for sliceheight := float32(0.0); sliceheight <= heighestz; sliceheight += conf.LayerHeight {
-    fmt.Printf("Layer level: %v\n", sliceheight)
-    var intersectingkeys []int
     for key := range model.Triangles {
-      if (model.Triangles[key].IntersectsZ(sliceheight)) {
-        intersectingkeys = append(intersectingkeys, key)
+      if model.Triangles[key].IntersectsZ(sliceheight) {
+        v1, v2 := model.Triangles[key].IntersectVectors(sliceheight)
+        linelist = append(linelist, geometry.LineSegment{V1: v1, V2: v2})
+        //fmt.Printf("Points Intersected: %v, %v\n", v1, v2)
       }
     }
-    fmt.Printf("Intersecting triangles: %v\n", intersectingkeys)
+    //fmt.Printf("%v", linelist)
+
+    geometry.Save2DSlice(linelist, "layer_" + utils.LeftPad2Len(strconv.Itoa(iterator), "0", 4) + ".png")
+    linelist = nil
+    iterator += 1
   }
 }
