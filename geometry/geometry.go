e76d71f45c9b800d02cf316582742f4530740fcc
MatanSilver <matansilver@gmail.com>
2017-01-11 19:58:06 -0500

    switched vectors to an array instead of discrete vectors

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -18,14 +18,55 @@ func (m *Model) Print() {
   }
 }
 
+func (m *Model) HeighestZ() float32 {
+  var highest float32 = 0.0
+  for key := range m.Triangles { //iterate triangles
+    for key2 := range m.Triangles[key].Vertices { //iterate vertices
+      if (m.Triangles[key].Vertices[key2][2] > highest) {
+        highest = m.Triangles[key].Vertices[key2][2]
+      }
+    }
+  }
+  return highest
+}
+
+func (m *Model) GetTarget() Vector {
+  return Vector{m.Triangles[0].Vertices[0][0], m.Triangles[0].Vertices[0][1], m.Triangles[0].Vertices[0][2]}
+}
+
 type Triangle struct {
   Normal      [3]float32
-  Vertex1     [3]float32
-  Vertex2     [3]float32
-  Vertex3     [3]float32
+  Vertices    [3]Vector
   Attribute   uint16
 }
 
 func (tri *Triangle) Print() {
-  fmt.Printf("Triangle\n\tNormal: %v\n\tVertex 1: %v\n\tVertex 2: %v\n\tVertex 3: %v\n\tAttribute: %v\n", tri.Normal, tri.Vertex1, tri.Vertex2, tri.Vertex3, tri.Attribute)
+  fmt.Printf("Triangle\n\tNormal: %v\n\tVertex 1: %v\n\tVertex 2: %v\n\tVertex 3: %v\n\tAttribute: %v\n", tri.Normal, tri.Vertices[0], tri.Vertices[1], tri.Vertices[2], tri.Attribute)
+}
+
+func (tri *Triangle) IntersectsZ(zlevel float32) bool {
+  if (tri.Vertices[0][2] > zlevel && tri.Vertices[1][2] > zlevel && tri.Vertices[2][2] > zlevel) {
+    return false
+  } else if (tri.Vertices[0][2] < zlevel && tri.Vertices[1][2] < zlevel && tri.Vertices[2][2] < zlevel) {
+    return false
+  }
+  return true
+}
+
+type Vector [3]float32
+
+func (vec *Vector) Cross(vec2 Vector) Vector {
+  return Vector{vec[1]*vec2[2] - vec[2]*vec2[1], -(vec[0]*vec2[2]-vec[2]*vec2[0]), vec[0]*vec2[1]-vec[1]*vec2[0]}
+}
+
+func (vec *Vector) Dot(vec2 Vector) float32 {
+  return float32(vec[0]*vec2[0] + vec[1]*vec2[1] + vec[2]*vec2[2])
+}
+
+func average(xs[]float32)float32 {
+	total:=float32(0.0)
+	for _,v:=range xs {
+		total += v
+	}
+	return total/float32(len(xs))
 }
