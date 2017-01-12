d206705ee6515fc6829a9d9e3813e8720c197d1e
MatanSilver <matansilver@gmail.com>
2017-01-11 20:00:20 -0500

    added some to slice function

--- slicer/slicer.go
+++ slicer/slicer.go
@@ -2,7 +2,18 @@ package slicer
 import (
   "../geometry"
   "../utils"
+  "fmt"
 )
 func Slice(filename string, model *geometry.Model, conf *utils.Config) {
-  
+  heighestz := model.HeighestZ()
+  for sliceheight := float32(0.0); sliceheight <= heighestz; sliceheight += conf.LayerHeight {
+    fmt.Printf("Layer level: %v\n", sliceheight)
+    var intersectingkeys []int
+    for key := range model.Triangles {
+      if (model.Triangles[key].IntersectsZ(sliceheight)) {
+        intersectingkeys = append(intersectingkeys, key)
+      }
+    }
+    fmt.Printf("Intersecting triangles: %v\n", intersectingkeys)
+  }
 }
