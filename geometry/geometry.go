4738080eabce8ea10f8930e741477ec768f4ab03
MatanSilver <matansilver@gmail.com>
2017-01-11 23:14:30 -0500

    added function for calculating intersects between a triangle and a z plane

--- geometry/geometry.go
+++ geometry/geometry.go
@@ -3,7 +3,6 @@ import (
   "fmt"
 )
 
-
 type Model struct {
   Header      [80]byte
   Count       uint32
@@ -49,11 +48,53 @@ func (tri *Triangle) IntersectsZ(zlevel float32) bool {
     return false
   } else if (tri.Vertices[0][2] < zlevel && tri.Vertices[1][2] < zlevel && tri.Vertices[2][2] < zlevel) {
     return false
+  } else if (tri.Vertices[0][2] == zlevel && tri.Vertices[1][2] == zlevel && tri.Vertices[2][2] == zlevel) {
+    return false
   }
   return true
 }
 
+func (tri *Triangle) IntersectVectors(zlevel float32) (Vector, Vector) {
+  //if two points on z level, return those points
+  if (tri.Vertices[0][2] == zlevel && tri.Vertices[1][2] == zlevel) {
+    return tri.Vertices[0], tri.Vertices[1]
+  } else if (tri.Vertices[1][2] == zlevel && tri.Vertices[2][2] == zlevel) {
+    return tri.Vertices[1], tri.Vertices[2]
+  } else if (tri.Vertices[0][2] == zlevel && tri.Vertices[2][2] == zlevel) {
+    return tri.Vertices[0], tri.Vertices[2]
+  } else { // otherwise, calculate the two intersections parametrically (could do rref?)
+    var va, vb, origin Vector
+    if (tri.Vertices[0][2] < zlevel && tri.Vertices[1][2] < zlevel) {
+      va = tri.Vertices[0]
+      vb = tri.Vertices[1]
+      origin = tri.Vertices[2]
+    } else if (tri.Vertices[1][2] < zlevel && tri.Vertices[2][2] < zlevel) {
+      va = tri.Vertices[1]
+      vb = tri.Vertices[2]
+      origin = tri.Vertices[0]
+    } else if (tri.Vertices[0][2] < zlevel && tri.Vertices[2][2] < zlevel){
+      va = tri.Vertices[0]
+      vb = tri.Vertices[2]
+      origin = tri.Vertices[1]
+    }
+    t1 := (zlevel - va[2])/(origin[2] - va[2])
+    t2 := (zlevel - vb[2])/(origin[2] - vb[2])
+    x1 := va[0] + t1*(origin[0] - va[0])
+    x2 := vb[0] + t2*(origin[0] - vb[0])
+    y1 := va[1] + t1*(origin[1] - va[1])
+    y2 := vb[1] + t2*(origin[1] - vb[1])
+    v1 := Vector{x1, y1, zlevel}
+    v2 := Vector{x2, y2, zlevel}
+    return v1, v2
+  }
+}
+
 type Vector [3]float32
+
+type LineSegment struct {
+  V1 Vector
+  V2 Vector
+}
 
 func (vec *Vector) Cross(vec2 Vector) Vector {
   return Vector{vec[1]*vec2[2] - vec[2]*vec2[1], -(vec[0]*vec2[2]-vec[2]*vec2[0]), vec[0]*vec2[1]-vec[1]*vec2[0]}
