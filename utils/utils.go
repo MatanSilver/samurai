b7e191edad08d288c35db69b42b1d605da121493
MatanSilver <matansilver@gmail.com>
2017-01-11 23:13:21 -0500

    added left pad function

--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,17 @@
 package utils
+import (
+  "strings"
+)
 
 func Check(e error) {
   if e != nil {
     panic(e)
   }
+}
+
+func LeftPad2Len(s string, padStr string, overallLen int) string {
+	var padCountInt int
+	padCountInt = 1 + ((overallLen - len(padStr)) / len(padStr))
+	var retStr = strings.Repeat(padStr, padCountInt) + s
+	return retStr[(len(retStr) - overallLen):]
 }
