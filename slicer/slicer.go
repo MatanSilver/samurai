96413eec96670a8458c24ac978b3f3db169f64fa
MatanSilver <matansilver@gmail.com>
2017-01-13 19:21:48 -0500

    fixed linelists

--- slicer/slicer.go
+++ slicer/slicer.go
@@ -2,26 +2,22 @@ package slicer
 import (
   "../geometry"
   "../utils"
-  //"fmt"
   "strconv"
 )
 
 func Slice(filename string, model *geometry.Model, conf *utils.Config) {
   heighestz := model.HeighestZ()
-  var linelist []geometry.LineSegment
+  linelists := [][]geometry.LineSegment{}
   iterator := 0
   for sliceheight := float32(0.0); sliceheight <= heighestz; sliceheight += conf.LayerHeight {
+    linelists = append(linelists, []geometry.LineSegment{})
     for key := range model.Triangles {
       if model.Triangles[key].IntersectsZ(sliceheight) {
         v1, v2 := model.Triangles[key].IntersectVectors(sliceheight)
-        linelist = append(linelist, geometry.LineSegment{V1: v1, V2: v2})
-        //fmt.Printf("Points Intersected: %v, %v\n", v1, v2)
+        linelists[iterator] = append(linelists[iterator], geometry.LineSegment{V1: v1, V2: v2})
       }
     }
-    //fmt.Printf("%v", linelist)
-
-    geometry.Save2DSlice(linelist, "layer_" + utils.LeftPad2Len(strconv.Itoa(iterator), "0", 4) + ".png")
-    linelist = nil
+    geometry.Save2DSlice(linelists[iterator], "layer_" + utils.LeftPad2Len(strconv.Itoa(iterator), "0", 4) + ".png")
     iterator += 1
   }
 }
