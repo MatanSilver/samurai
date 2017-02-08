59157ed936219c4c1f8aff588d31b478c467db8b
MatanSilver <matansilver@gmail.com>
2017-02-08 11:18:11 -0500

    wrote some rotation functions

--- geometry/vector.go
+++ geometry/vector.go
@@ -1,5 +1,9 @@
 package geometry
 
+import (
+  "math"
+)
+
 type Vector [3]float64
 
 type Vector32 [3]float32
@@ -17,11 +21,32 @@ func (vec *Vector) Dot(vec2 Vector) float64 {
   return vec[0]*vec2[0] + vec[1]*vec2[1] + vec[2]*vec2[2]
 }
 
-func (vec *Vector) Rotate(rotation Vector, origin Vector) Vector {
-return *vec
+func (vec *Vector) Add(vec2 Vector) *Vector {
+  return &Vector{vec[0] + vec2[0], vec[1] + vec2[1], vec[2] + vec2[2]}
 }
 
-func (vec *Vector) RotateX(angle float64) Vector{
-  //mat := Matrix{[]float64{1.0, 0.0, 0.0}, []float64{0, math.Cos(angle), -math.Sin(angle)}, []float64{0, math.Sin(angle), math.Cos(angle)}}
-  return *vec
+func (vec *Vector) Subtract(vec2 Vector) *Vector {
+  return &Vector{vec[0] - vec2[0], vec[1] - vec2[1], vec[2] - vec2[2]}
+}
+
+func (vec *Vector) Rotate(rot Vector, origin Vector) *Vector {
+  return vec.Subtract(origin).RotateX(rot[0]).RotateY(rot[1]).RotateZ(rot[2]).Add(origin)
+}
+
+func (vec *Vector) RotateX(angle float64) *Vector {
+  rotation_mat := Matrix{[]float64{1.0, 0.0, 0.0}, []float64{0, math.Cos(angle), -math.Sin(angle)}, []float64{0, math.Sin(angle), math.Cos(angle)}}
+  rotated_vec := MatrixMultiply(rotation_mat, [][]float64{[]float64{vec[0]}, []float64{vec[1]}, []float64{vec[2]}})
+  return &Vector{rotated_vec[0][0], rotated_vec[0][1], rotated_vec[0][2]}
+}
+
+func (vec *Vector) RotateY(angle float64) *Vector{
+  rotation_mat := Matrix{[]float64{math.Cos(angle), 0.0, math.Sin(angle)}, []float64{0.0, 1.0, 0.0}, []float64{-math.Sin(angle), 0.0, math.Cos(angle)}}
+  rotated_vec := MatrixMultiply(rotation_mat, [][]float64{[]float64{vec[0]}, []float64{vec[1]}, []float64{vec[2]}})
+  return &Vector{rotated_vec[0][0], rotated_vec[0][1], rotated_vec[0][2]}
+}
+
+func (vec *Vector) RotateZ(angle float64) *Vector{
+  rotation_mat := Matrix{[]float64{math.Cos(angle), -math.Sin(angle), 0.0}, []float64{math.Sin(angle), math.Cos(angle), 0.0}, []float64{0.0, 0.0, 1.0}}
+  rotated_vec := MatrixMultiply(rotation_mat, [][]float64{[]float64{vec[0]}, []float64{vec[1]}, []float64{vec[2]}})
+  return &Vector{rotated_vec[0][0], rotated_vec[0][1], rotated_vec[0][2]}
 }
